01ecdde182a033df23d5dc4973704750ebb844d5
Vishwanathan27 <63465722+Vishwanathan27@users.noreply.github.com>
2023-08-29 12:42:27 +0530

    Update aws.js

--- src/config/aws.js
+++ src/config/aws.js
@@ -1,5 +1,5 @@
 module.exports = {
   AWS_ACCESS_SECRET: process.env.AWS_ACCESS_SECRET || "changeme",
-  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY || "changeme",
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_SECRET || "changeme",
   BUCKET_NAME: process.env.AWS_BUCKET_NAME || "changeme",
 };
